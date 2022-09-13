82c1d0c7dbf87ab38e07d4e5fa921dd6a536f58f
Yunus Gürlek <ygurlek22@my.uaa.k12.tr>
2022-09-13 11:44:10 +0200

    Add responsive design for header

--- public/js/functions/header.js
+++ public/js/functions/header.js
@@ -14,5 +14,24 @@ window.addEventListener('load', () => {
     } else if (document.querySelector('.each-all-header-menu-wrapper-opened')) {
       document.querySelector('.each-all-header-menu-wrapper-opened').classList.remove('each-all-header-menu-wrapper-opened');
     }
+  });
+
+  document.addEventListener('click', event => {
+    if (ancestorWithClassName(event.target, 'responsive-header-open-menu-button')) {
+      if (document.querySelector('.all-header-menu-wrapper-responsive-outer-wrapper').style.display == 'none') {
+        document.querySelector('.all-header-menu-wrapper-responsive-outer-wrapper').style.display = 'flex';
+      } else {
+        document.querySelector('.all-header-menu-wrapper-responsive-outer-wrapper').style.display = 'none';
+      }
+    }
+
+    if (ancestorWithClassName(event.target, 'each-all-header-menu-title-wrapper-responsive')) {
+      const target = ancestorWithClassName(event.target, 'each-all-header-menu-title-wrapper-responsive');
+
+      if (document.querySelector('.each-all-header-menu-opened'))
+        document.querySelector('.each-all-header-menu-opened').classList.remove('each-all-header-menu-opened');
+
+      target.nextElementSibling.classList.add('each-all-header-menu-opened');
+    }
   })
-})
+});
